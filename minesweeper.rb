5a3806f241cbe8f2b67672417195b84b76ff1f90
Malcolm Carradine <malcolm.carradine@gmail.com>
2014-07-14 10:57:53 -0400

    Add Board and Tile classes

---
+++ minesweeper.rb
@@ -0,0 +1,17 @@
+class Minesweeper
+
+end
+
+class Board
+
+  def initialize(board_dimension = 9)
+    @board = Array.new(board_dimension) { Array.new(board_dimension)}
+  end
+
+
+
+end
+
+class Tile
+
+end
