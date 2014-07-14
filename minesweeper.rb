7822e356cb6eedf7258e59f2700b9ffbbfc06a94
Malcolm Carradine <malcolm.carradine@gmail.com>
2014-07-14 11:23:09 -0400

    Add neighbor_bomb_count, board.tile

--- minesweeper.rb
+++ minesweeper.rb
@@ -4,10 +4,15 @@ end
 
 class Board
 
+  attr_accessor :board
+
   def initialize(board_dimension = 9)
     @board = Array.new(board_dimension) { Array.new(board_dimension)}
   end
 
+  def tile(coords)
+    @board[coords.last][coords.first]
+  end
 
 
 end
@@ -32,20 +37,31 @@ class Tile
             ]
 
   def reveal
-
+    revealed? = true
   end
 
   def neighbors
     my_neighbors = []
+
     NEIGHBOR.each do |neighbor|
       my_neighbors << [(self.coords.first - neighbor.first), (self.coords.last - neighbor.last)]
     end
+
     my_neighbors.select { |coord| coord.none? { |num| num > 8 || num < 0 }
-    my_neighbors = my_neighbor.map { |coord| self.board.board.tile(coord) }
+
+    my_neighbors = my_neighbors.map { |coord| self.board.tile(coord) }
   end
 
   def neighbor_bomb_count
+    adj_bombs = 0
 
+    self.neighbors.each do |neighbor|
+      if neighbor.bombed?
+        adj_bombs += 1
+      end
+    end
+
+    adj_bombs
   end
 
 end
